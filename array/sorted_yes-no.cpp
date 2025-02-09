1ffe1a7d5761779468e03416266135496efdbedc
MANR255926 <m.allyanadeem@gmail.com>
2025-02-09 20:19:09 +0500

    Add program to check if an array is sorted in ascending order

---
+++ array/sorted_yes-no.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {5, 10, 15, 20, 25};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    bool isSorted = true;
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])// if first element is bigger then scond it means not sorted 
+        {
+            isSorted = false;
+            break;
+        }
+    }
+
+    if (isSorted) {
+        cout << "The array is sorted in ascending order." << endl;
+    } else {
+        cout << "The array is not sorted in ascending order." << endl;
+    }
+
+    return 0;
+}
