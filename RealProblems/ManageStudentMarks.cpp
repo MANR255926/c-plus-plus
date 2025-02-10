70c53b2147014681a2342a93085b48c1e800812f
MANR255926 <m.allyanadeem@gmail.com>
2025-02-10 23:45:52 +0500

    Add ManageStudentMarks.cpp to find and display the lowest marks in a class

---
+++ RealProblems/ManageStudentMarks.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+// Function to find the lowest marks
+int findMin(int marks[], int size) {
+    int min = marks[0];
+    for(int i = 1; i < size; i++) {
+        if(marks[i] < min) {
+            min = marks[i];
+        }
+    }
+    return min;
+}
+int main() {
+    const int NUM_STUDENTS = 8;
+    int marks[NUM_STUDENTS];
+    
+    // Input marks for each student
+    for(int i = 0; i < NUM_STUDENTS; i++) {
+        cout << "Enter marks for student " << i + 1 << ": ";
+        cin >> marks[i];
+    }
+
+    // Find and display the lowest marks
+    int lowestMarks = findMin(marks, NUM_STUDENTS);
+    cout << "Lowest marks in the class: " << lowestMarks << endl;
+
+    return 0;
+}
